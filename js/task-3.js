506bedba07e0296e5bc35b5cec5e2baf9976c014
Derkach Nadiia <derkachnadiia1@gmail.com>
2025-04-02 11:43:10 +0300

    add all files

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,8 @@ const inputText = document.querySelector('#name-input');
 const spanAnon = document.querySelector('#name-output');
 inputText.addEventListener('input', event => {
   const inputValue = event.target.value.trim();
-  spanAnon.textContent === ''
+  console.log(inputValue);
+  inputValue === ''
     ? (spanAnon.textContent = 'Anonymous')
     : (spanAnon.textContent = inputValue);
 });
